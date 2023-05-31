213a232aa7bc166edd7ea23c978b7690213407e6
chengxiong.zhu <chengxiong.zhu@megatronix.co>
2023-05-31 17:45:57 +0800

    auto

--- pythonscript/AutoCode/adas_info/{fileName}.cpp
+++ pythonscript/AutoCode/adas_info/{fileName}.cpp
@@ -8,7 +8,7 @@ void {ClassName}::state_process(const SignalMsg &sig_msg)
     union veh_signal_value raw_value;
     if (!GetCommonSigPhysicalValue(raw_value, &CANSIG_{1}_g))
     {
-        TB_LOG_ERROR("get siginal value error. return");
+        IC_LOG_ERROR("get siginal value error. return");
         return;
     }
 
