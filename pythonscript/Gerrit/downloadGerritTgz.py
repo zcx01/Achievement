72a9fea587c2bff55b68822470c3b115c6ce3dc9
chengxiong.zhu <chengxiong.zhu@megatronix.co>
2023-06-16 16:23:13 +0800

    downloadGerrit

---
+++ pythonscript/Gerrit/downloadGerritTgz.py
@@ -0,0 +1,74 @@
+#!/bin/python
+import os
+import sys
+import argparse
+from commonfun import *
+from basic_auth import *
+import requests
+import shutil
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+pyFileDir = os.path.dirname(os.path.abspath(__file__))+"/"
+# headers={"Cookie":"GerritAccount=aUsfprsWMFK5HiIaJY1RHhM2KHQOAilL6a; XSRF_TOKEN=aUsfprtm9B.c6C1tHvjio-KbtDjFvSjOrW"}
+'''
+下载 chromedriver
+https://registry.npmmirror.com/binary.html?path=chromedriver/
+'''
+
+def GetCookie(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(executable_path=pyFileDir+"/chromedriver",options=options)
+    driver.get(url)
+    driver.find_element("xpath",'/html/body/header/div/div[2]/a[2]').click()
+    time.sleep(1)
+    driver.find_element("id","f_user").send_keys(basic_auth[0])
+    driver.find_element("id",'f_pass').send_keys(basic_auth[1])
+    driver.find_element("id",'b_signin').click()
+    time.sleep(1)
+    driverCookies = driver.get_cookies()
+    driverCookieslists=[]
+    for driverCookie in driverCookies:
+        driverCookieslists.append(f'{driverCookie["name"]}={driverCookie["value"]}')
+    driverCookies ="; ".join(driverCookieslists)
+    cookies={}
+    cookies["Cookie"]=driverCookies
+    print(cookies)
+    return cookies
+       
+def DownloadGerritTgz(url,path):
+    assert isinstance(url,str)
+    # url = "https://git.i-tetris.com/plugins/gitiles/qcom/sa/qnx/mega/prebuilts/bigsur/+/refs/heads/sop/bigsur_hqx.1.2.1_20230130"
+    # wget.download(url,"./bigsur_hqx.1.2.1_20230130.tar.gz")
+    urls = url.split("/")
+    fileName = urls[len(urls)-1]
+    file = path+"/"+fileName
+    print("下载 "+url)
+    headers = GetCookie(url)
+    response = requests.get(url,auth=basic_auth,allow_redirects=True,headers=headers)
+    content = response.content
+    print("写入 "+file)
+    with open(file, "wb") as cr:
+        cr.write(content)
+    print("解压中 "+file)
+    tagName = fileName.split(".")
+    if len(tagName) !=0:
+        tagName = tagName[0]
+    tagName=path+"/"+tagName
+    if os.path.exists(tagName):
+        shutil.rmtree(tagName)
+    os.makedirs(tagName)
+    os.system(f"tar -zxvf {file} -C {tagName}")
+    print("下载完成")
+
+if __name__ == "__main__":
+    parse = argparse.ArgumentParser(description='下载GerritTgz')
+    parse.add_argument('-u','--url',help='url',type=str)
+    parse.add_argument('-p','--path',help='url',type=str,default=".",nargs="?")
+    arg = parse.parse_args()
+    DownloadGerritTgz(arg.url,arg.path)
